e6d987044797c7f6c3db57122e7a813d86636aa8
bitao666 <15205192775@163.com>
2021-10-22 11:31:45 +0800

    new file

---
+++ src/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[]) {
+    int input_value = atof(argv[1]);
+    std::cout << "Hello, World!" << input_value << std::endl;
+    return 0;
+}
